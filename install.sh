96db77b2dce0d00c6f0fc583f24d1e0822085397
Tan Le <tanle2207@gmail.com>
2021-07-20 20:49:34 +0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update && apt-get upgrade;
+apt-get install -y python3-pip neovim nodejs npm stow;
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
+# install vim-plug
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
+
