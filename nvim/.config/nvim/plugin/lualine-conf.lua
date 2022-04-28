00c3d7ca89167f6fdbe9f50bcc8c5f3b6b36b0f2
Tan Le <tanle2207@gmail.com>
2022-04-28 16:04:08 +0700

    Change lualine sections

--- nvim/.config/nvim/plugin/lualine-conf.lua
+++ nvim/.config/nvim/plugin/lualine-conf.lua
@@ -1,10 +1,17 @@
 local gps = require("nvim-gps")
 gps.setup()
 require("lualine").setup({
-sections = {
-	lualine_c = {
-		'filename',
-		{ gps.get_location, cond = gps.is_available },
-		}
-	}
+  sections = {
+    lualine_b = {'branch'},
+    lualine_c = {
+      'filename',
+      { gps.get_location, cond = gps.is_available },
+      },
+    lualine_x = {'filetype'},
+    lualine_y = {'diff', 'diagnostics'},
+  },
+  inactive_sections = {
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+  },
+  options = { theme = 'gruvbox' },
 })
