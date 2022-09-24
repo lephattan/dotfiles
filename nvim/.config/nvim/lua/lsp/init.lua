d21c6b69548e6e798d5a1d9a72c1db5c7dc29f98
Tan Le <tanle2207@gmail.com>
2022-09-24 16:36:30 +0700

    Add ccls

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -13,6 +13,7 @@ function M.setup()
     vimls = {},
     --sumneko_lua = {},
     jsonls = {},
+    ccls = {},
   }
 
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
