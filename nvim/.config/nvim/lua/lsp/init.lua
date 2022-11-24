ec7499cb6b800c663d75a259d5a36dbc7e896472
Tan Le <tanle2207@gmail.com>
2022-11-24 13:58:56 +0700

    Change update_capabilities to default_capabilities

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -20,7 +20,7 @@ function M.setup()
     dockerls = {},
   }
 
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
   local function on_attach(client, bufnr)
     vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
