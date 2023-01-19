11ae58780e4f0078ab4c5ed4ebed14e8982c3a95
Tan Le <tanle2207@gmail.com>
2023-01-19 14:12:48 +0700

    Change cmp buffer keyword length

--- nvim/.config/nvim/lua/nvim-cmp.lua
+++ nvim/.config/nvim/lua/nvim-cmp.lua
@@ -2,36 +2,36 @@
 local cmp = require('cmp')
 
 cmp.setup({
-	snippet = {
+  snippet = {
     expand = function(args)
       vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
     end
-	},
-	mapping = {
-		['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-		['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-		-- ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+  },
+  mapping = {
+    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    -- ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
     ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
     ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-    ['<C-y>'] = cmp.mapping(cmp.mapping.confirm{
+    ['<C-y>'] = cmp.mapping(cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
-    }, {'i', 'c'}),
-		['<C-e>'] = cmp.mapping({
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		}),
+    }, { 'i', 'c' }),
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
     ['<CR>'] = cmp.mapping.confirm({
       select = true,
       behavior = cmp.ConfirmBehavior.Replace,
     }),
-	},
+  },
   sources = {
-    {name = 'ultisnips'},
-    {name = 'buffer', keyword_length = 5},
-    {name = 'nvim_lsp'},
-    {name = 'path'},
+    { name = 'ultisnips' },
+    { name = 'buffer', keyword_length = 3 },
+    { name = 'nvim_lsp' },
+    { name = 'path' },
     -- {name = 'treesitter'},
     -- {name = 'nvim_lua'},
   },
@@ -45,20 +45,20 @@ cmp.setup({
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
-	sources = {
-		{ name = 'buffer' }
-	},
+  sources = {
+    { name = 'buffer' }
+  },
   mapping = cmp.mapping.preset.cmdline(),
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-	sources = cmp.config.sources({
-		{ name = 'path' }
-	}, 
-  {
-		{ name = 'cmdline' }
-	}),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  },
+    {
+      { name = 'cmdline' }
+    }),
   mapping = cmp.mapping.preset.cmdline(),
 })
 
