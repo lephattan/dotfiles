f3ec5edab5c651bbf0568362977b962454daeb58
Tan Le <tanle2207@gmail.com>
2023-01-19 10:41:25 +0700

    Format lua files

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -2,38 +2,38 @@ local api = vim.api
 local create_autocmd = api.nvim_create_autocmd
 
 api.nvim_create_autocmd(
-{'BufNewFile', 'BufRead'}, {
-  pattern={'*.py'},
-  command=[[
-  setlocal tabstop=4 shiftwidth=4 expandtab autoindent fileformat=unix colorcolumn=100
+  { 'BufNewFile', 'BufRead' }, {
+  pattern = { '*.py' },
+  command = [[
+  setlocal tabstop=4 shiftwidth=4 expandtab autoindent fileformat=unix colorcolumn=88
   nnoremap <silent> <leader>dn :lua require('dap-python').test_method()<CR>
   nnoremap <silent> <leader>df :lua require('dap-python').test_class()<CR>
   ]]
 })
 
-create_autocmd({'FileType'}, {
-  pattern={'vue'},
+create_autocmd({ 'FileType' }, {
+  pattern = { 'vue' },
   -- command=[[setlocal sts=0 expandtab|set filetype=vue.html]],
-  command=[[setlocal sts=0 expandtab|set filetype=vue]],
+  command = [[setlocal sts=0 expandtab|set filetype=vue]],
 })
 
 
-create_autocmd({'FileType'}, {
-  pattern={'php'},
-  command=[[setlocal ai si]],
+create_autocmd({ 'FileType' }, {
+  pattern = { 'php' },
+  command = [[setlocal ai si]],
 })
 
-create_autocmd({'FileType'}, {
-  pattern={'markdown'},
-  command=[[setlocal wrap linebreak]],
+create_autocmd({ 'FileType' }, {
+  pattern = { 'markdown' },
+  command = [[setlocal wrap linebreak]],
 })
 
-create_autocmd({'BufNewFile', 'BufRead'}, {
-  pattern={'*.conf.template'},
-  command=[[set filetype=conf]],
+create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  pattern = { '*.conf.template' },
+  command = [[set filetype=conf]],
 })
 
-create_autocmd({'FileType'}, {
-  pattern={'liquid'},
-  command=[[setlocal sts=0 expandtab|set filetype=liquid.html]],
+create_autocmd({ 'FileType' }, {
+  pattern = { 'liquid' },
+  command = [[setlocal sts=0 expandtab|set filetype=liquid.html]],
 })
