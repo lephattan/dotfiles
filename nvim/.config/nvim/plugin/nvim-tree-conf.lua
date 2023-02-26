eb4e58a4f3fc4c048eae8cd64a8c2eed9acbc4cf
Tan Le <tanle2207@gmail.com>
2023-02-26 09:53:14 +0700

    Change nvim-tree width

--- nvim/.config/nvim/plugin/nvim-tree-conf.lua
+++ nvim/.config/nvim/plugin/nvim-tree-conf.lua
@@ -1,21 +1,21 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require 'nvim-tree'.setup {
-  git = {
+  git                 = {
     ignore = false,
   },
-  actions = {
-	  open_file = {
-		  quit_on_open = true,
-		  window_picker = {
-			  enable = true,
-			  exclude = {
-				  filetype = {'notify', 'packer', 'qf'},
-				  buftype = {'terminal'}
-			  },
-		  },
+  actions             = {
+    open_file = {
+      quit_on_open = true,
+      window_picker = {
+        enable = true,
+        exclude = {
+          filetype = { 'notify', 'packer', 'qf' },
+          buftype = { 'terminal' }
+        },
+      },
       resize_window = false,
-	  }
+    }
   },
   disable_netrw       = true,
   hijack_netrw        = true,
@@ -26,10 +26,10 @@ require 'nvim-tree'.setup {
   hijack_cursor       = false,
   update_cwd          = true,
   --update_to_buf_dir   = {
-    --enable = true,
-    --auto_open = true,
+  --enable = true,
+  --auto_open = true,
   --},
-  diagnostics = {
+  diagnostics         = {
     enable = false,
     icons = {
       hint = "",
@@ -43,16 +43,16 @@ require 'nvim-tree'.setup {
     update_cwd  = true,
     ignore_list = {}
   },
-  system_open = {
+  system_open         = {
     cmd  = nil,
     args = {}
   },
-  filters = {
+  filters             = {
     dotfiles = false,
     custom = {}
   },
-  view = {
-    width = 30,
+  view                = {
+    width = 40,
     -- height = 30,
     hide_root_folder = false,
     side = 'left',
@@ -62,10 +62,9 @@ require 'nvim-tree'.setup {
       list = {}
     }
   },
-  renderer = {
+  renderer            = {
     indent_markers = {
       enable = true,
     },
   },
 }
-
