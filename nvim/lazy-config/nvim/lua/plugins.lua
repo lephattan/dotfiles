e527a427b05003d03ef52304a771b52b6cce1c77
Tan Le <tanle2207@gmail.com>
2023-06-06 14:00:13 +0700

    feat: add config for treesitter

--- nvim/lazy-config/nvim/lua/plugins.lua
+++ nvim/lazy-config/nvim/lua/plugins.lua
@@ -19,6 +19,28 @@ require('lazy').setup({
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ':TSUpdate',
+    opts = {
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+      context_commentstring = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+        disable = { 'python' }
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<c-space>',
+          node_incremental = '<c-space>',
+          scope_incremental = '<c-s>',
+          node_decremental = '<M-space>',
+        },
+      },
+    }
   },
 
   {
