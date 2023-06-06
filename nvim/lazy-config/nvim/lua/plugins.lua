e3d55e3d097a2daa8d8bc8b5c2678173cb1d643d
Tan Le <tanle2207@gmail.com>
2023-06-06 14:04:56 +0700

    feat: add null-ls

--- nvim/lazy-config/nvim/lua/plugins.lua
+++ nvim/lazy-config/nvim/lua/plugins.lua
@@ -383,6 +383,23 @@ require('lazy').setup({
       }
     end
   },
+  {
+    'jose-elias-alvarez/null-ls.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local null_ls = require('null-ls')
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.diagnostics.flake8,
+          null_ls.builtins.code_actions.gitsigns,
+          null_ls.builtins.diagnostics.eslint_d,
+          null_ls.builtins.formatting.prettierd,
+          null_ls.builtins.formatting.black,
+          null_ls.builtins.code_actions.eslint_d,
+        },
+      })
+    end
+  },
 
   require 'lsp-debug',
   require 'autoformat',
