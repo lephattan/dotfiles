a4369aa5fcb5dabfe017c8031da74171e657451a
Tan Le <tanle2207@gmail.com>
2023-09-16 14:58:44 +0700

    lazy-nvim: tweak treesitter context

--- nvim/lazy-config/nvim/lua/plugins.lua
+++ nvim/lazy-config/nvim/lua/plugins.lua
@@ -255,7 +255,7 @@ require('lazy').setup({
       show_trailing_blankline_indent = false,
       space_char_blankline = " ",
       show_current_context = true,
-      show_current_context_start = true,
+      show_current_context_start = false,
     },
   },
   {
