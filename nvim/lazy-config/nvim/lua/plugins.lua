43975e5eef78c2a9b5bf81512e978a4cb296a2bd
Tan Le <tanle2207@gmail.com>
2023-10-12 15:26:04 +0700

    fix: disable null_ls prettierd on html files due to broken templates

--- nvim/lazy-config/nvim/lua/plugins.lua
+++ nvim/lazy-config/nvim/lua/plugins.lua
@@ -436,7 +436,7 @@ require('lazy').setup({
           null_ls.builtins.diagnostics.flake8,
           null_ls.builtins.code_actions.gitsigns,
           null_ls.builtins.diagnostics.eslint_d,
-          null_ls.builtins.formatting.prettierd,
+          null_ls.builtins.formatting.prettierd.with { disabled_filetypes = { 'html' } },
           null_ls.builtins.formatting.black,
           null_ls.builtins.code_actions.eslint_d,
         },
