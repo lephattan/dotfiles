562583fcf946476d5c828383101cfd746db2fe6c
Tan Le <tanle2207@gmail.com>
2024-02-02 11:53:21 +0700

    feat: rm emmeet-ls

--- nvim/lazy-config/nvim/lua/lsp.lua
+++ nvim/lazy-config/nvim/lua/lsp.lua
@@ -80,18 +80,6 @@ local servers = {
   yamlls = {},
   gopls = {},
   sqlls = {},
-  emmet_ls = {
-    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug",
-      "typescriptreact", "vue" },
-    init_options = {
-      html = {
-        options = {
-          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-          ["bem.enabled"] = true,
-        },
-      },
-    }
-  },
   clangd = {},
 }
 
