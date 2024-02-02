b9f89919c8f2ffb4297869f001b7fca6b456d486
Tan Le <tanle2207@gmail.com>
2024-02-02 11:40:38 +0700

    feat: migrate from null-ls to none-ls

--- nvim/lazy-config/nvim/lua/plugins.lua
+++ nvim/lazy-config/nvim/lua/plugins.lua
@@ -446,30 +446,45 @@ require('lazy').setup({
     end
   },
   {
-    'jose-elias-alvarez/null-ls.nvim',
+    -- 'jose-elias-alvarez/null-ls.nvim',
+    'nvimtools/none-ls.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local null_ls = require('null-ls')
+
       local function style50()
         local h = require("null-ls.helpers")
         local methods = require("null-ls.methods")
         local FORMATTING = methods.internal.FORMATTING
+        local RANGE_FORMATTING = methods.internal.RANGE_FORMATTING
 
         return h.make_builtin {
           name = "style50_format",
           meta = {},
-          method = FORMATTING,
+          method = { FORMATTING, RANGE_FORMATTING },
           filetypes = { "c" },
           generator_opts = {
             command = "clang-format",
-            args = {
-              "-style={ AllowShortFunctionsOnASingleLine: Empty, BraceWrapping: { AfterCaseLabel: true, AfterControlStatement: true, AfterFunction: true, AfterStruct: true, BeforeElse: true, BeforeWhile: true }, BreakBeforeBraces: Custom, ColumnLimit: 132, IndentCaseLabels: true, IndentWidth: 4, SpaceAfterCStyleCast: true, TabWidth: 4 }",
-              "$FILENAME",
-            },
+            -- args = {
+            --   "-style={ AllowShortFunctionsOnASingleLine: Empty, BraceWrapping: { AfterCaseLabel: true, AfterControlStatement: true, AfterFunction: true, AfterStruct: true, BeforeElse: true, BeforeWhile: true }, BreakBeforeBraces: Custom, ColumnLimit: 132, IndentCaseLabels: true, IndentWidth: 4, SpaceAfterCStyleCast: true, TabWidth: 4 }",
+            --   "-assume-filename", "$FILENAME",
+            --   "--offset",
+            --   "--length",
+            -- },
+            args = h.range_formatting_args_factory(
+              { "-assume-filename", "$FILENAME",
+                "-style",
+                "{ AllowShortFunctionsOnASingleLine: Empty, BraceWrapping: { AfterCaseLabel: true, AfterControlStatement: true, AfterFunction: true, AfterStruct: true, BeforeElse: true, BeforeWhile: true }, BreakBeforeBraces: Custom, ColumnLimit: 132, IndentCaseLabels: true, IndentWidth: 4, SpaceAfterCStyleCast: true, TabWidth: 4 }" },
+              "--offset",
+              "--length",
+              { use_length = true, row_offset = -1, col_offset = -1 }
+            ),
+            to_stdin = true,
           },
           factory = h.formatter_factory,
         }
       end
+
       null_ls.setup({
         sources = {
           null_ls.builtins.diagnostics.flake8,
@@ -478,6 +493,7 @@ require('lazy').setup({
           null_ls.builtins.code_actions.eslint_d,
           null_ls.builtins.formatting.prettierd.with { disabled_filetypes = { 'html' } },
           null_ls.builtins.formatting.black,
+          -- null_ls.builtins.formatting.clang_format,
           style50,
         },
       })
