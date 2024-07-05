9b3a38798ef8b00cb8bea52c2e602c92a474c135
Tan Le <tanle2207@gmail.com>
2024-07-05 21:55:04 +0700

    nvim(lazy): turn off auto update checking

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -28,10 +28,11 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   -- install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
   ui = {
     border = "rounded"
-  }
+  },
+
 })
 vim.cmd.colorscheme "catppuccin"
 
