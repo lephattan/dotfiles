caf3a17d2e0111a2ae706449462fb99957618b37
Tan Le <tanle2207@gmail.com>
2024-08-19 09:09:11 +0700

    nvim(markdown): add "markview"

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,8 +1,46 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  build = "cd app && npm install",
-  config = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  ft = "markdown",
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    config = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = "markdown",
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+    dependencies = {
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+      require("markview").setup({
+        -- headings = {
+        --   heading_2 = {
+        --     padding_left = " ",
+        --   },
+        --   heading_3 = {
+        --     padding_left = " ",
+        --   },
+        --   heading_4 = {
+        --     padding_left = " ",
+        --   },
+        -- },
+        modes = { "n", "no", "c", "i" }, -- Change these modes to what you need
+        hybrid_modes = { "n", "i" },     -- Uses this feature on normal mode
+        -- This is nice to have
+        callbacks = {
+          on_enable = function(_, win)
+            vim.wo[win].conceallevel = 2;
+            vim.wo[win].conecalcursor = "c";
+          end
+        }
+      })
+    end,
+  },
 }
