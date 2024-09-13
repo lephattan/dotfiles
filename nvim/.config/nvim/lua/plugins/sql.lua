ed8fe477d7f9ab8530a921a805c7c35cefcb2e51
Tan Le <tan.lp@zwilling.com.vn>
2024-09-13 10:16:08 +0700

    nvim(sql): add Count helper for mysql

--- nvim/.config/nvim/lua/plugins/sql.lua
+++ nvim/.config/nvim/lua/plugins/sql.lua
@@ -1,5 +1,15 @@
 return {
   "tpope/vim-dadbod",
   "kristijanhusak/vim-dadbod-completion",
-  "kristijanhusak/vim-dadbod-ui",
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    config = function()
+      vim.g.db_ui_execute_on_save = 0
+      vim.g.db_ui_table_helpers = {
+        mysql = {
+          Count = "Select Count(*) From {table}"
+        }
+      }
+    end
+  },
 }
