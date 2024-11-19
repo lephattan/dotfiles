3860943ac38a9584bee4c855c6649531f4c3daa6
Tan Le <tanle2207@gmail.com>
2024-11-19 08:36:56 +0700

    wezterm: add wezterm config file

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.color_scheme = 'Catppuccin Mocha'
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+
+-- and finally, return the configuration to wezterm
+return config
