b21c7d1681e410303c5f803394c9b6b9c3f67fe5
tanle <tanle2207@gmail.com>
2025-02-25 12:23:29 +0700

    feat(nvim): clean and upgrade plugins

--- nvim/.config/nvim/lua/plugins/http.lua
+++ nvim/.config/nvim/lua/plugins/http.lua
@@ -3,19 +3,19 @@ return {
     "nvim-neotest/nvim-nio",
     lazy = true
   },
-  {
-    "rest-nvim/rest.nvim",
-    config = function()
-      ---@type rest.Opts
-      vim.g.rest_nvim = {
-        rocks = {
-          enabled = false
-        }
-      }
-      vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<CR>", { desc = "[R]est [r]un under cursor" })
-      vim.keymap.set("n", "<leader>rl", "<cmd>Rest run last<CR>", { desc = "[R]est run [l]ast request" })
-      vim.keymap.set("n", "<leader>rs", "<cmd>Rest env select<CR>", { desc = "[R]est env [s]elect " })
-    end,
-    lazy = true
-  },
+  -- {
+  --   "rest-nvim/rest.nvim",
+  --   config = function()
+  --     ---@type rest.Opts
+  --     vim.g.rest_nvim = {
+  --       rocks = {
+  --         enabled = false
+  --       }
+  --     }
+  --     vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<CR>", { desc = "[R]est [r]un under cursor" })
+  --     vim.keymap.set("n", "<leader>rl", "<cmd>Rest run last<CR>", { desc = "[R]est run [l]ast request" })
+  --     vim.keymap.set("n", "<leader>rs", "<cmd>Rest env select<CR>", { desc = "[R]est env [s]elect " })
+  --   end,
+  --   lazy = true
+  -- },
 }
--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -31,15 +31,17 @@ return {
         --     padding_left = " ",
         --   },
         -- },
-        modes = { "n", "no", "c", "i" }, -- Change these modes to what you need
-        hybrid_modes = { "n", "i" },     -- Uses this feature on normal mode
-        -- This is nice to have
-        callbacks = {
-          on_enable = function(_, win)
-            vim.wo[win].conceallevel = 2;
-            vim.wo[win].conecalcursor = "c";
-          end
+        preview = {
+          modes = { "n", "no", "c", "i" }, -- Change these modes to what you need
+          hybrid_modes = { "n", "i" },   -- Uses this feature on normal mode
+          callbacks = {
+            on_enable = function(_, win)
+              vim.wo[win].conceallevel = 2;
+              vim.wo[win].conecalcursor = "c";
+            end
+          }
         }
+        -- This is nice to have
       })
     end,
   },
