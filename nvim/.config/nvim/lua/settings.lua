501ffd94f8fbe41fb43645aeb4588408dba92835
tanle <tanle2207@gmail.com>
2025-11-30 13:52:02 +0700

    fix(nvim): diagnostic sign definition

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -60,7 +60,7 @@ api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
