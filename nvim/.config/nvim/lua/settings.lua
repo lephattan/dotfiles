51c5f73ee02209556a3fce6dd09121beee14f28b
tanle <tanle2207@gmail.com>
2025-11-30 13:53:31 +0700

    tweak(nvim): sql: change ftplugin keymap in favour of <C-c> in insert mode

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -65,3 +65,5 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+g.ftplugin_sql_omni_key = '<C-q>'
