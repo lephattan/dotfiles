183211df11cc67771dad341530a5242b56272abc
tanle <tanle2207@gmail.com>
2025-12-27 10:25:54 +0700

    dev(nvim): refactor and add mapping for DBUI

--- nvim/.config/nvim/lua/plugins/sql.lua
+++ nvim/.config/nvim/lua/plugins/sql.lua
@@ -1,15 +1,29 @@
 return {
-  "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-completion",
-  {
-    "kristijanhusak/vim-dadbod-ui",
-    config = function()
-      vim.g.db_ui_execute_on_save = 0
-      vim.g.db_ui_table_helpers = {
-        mysql = {
-          Count = "Select Count(*) From {table}"
-        }
-      }
-    end
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod',                     lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
   },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_execute_on_save = 0
+    vim.g.db_ui_table_helpers = {
+      mysql = {
+        Count = "Select Count(*) From {table}"
+      }
+    }
+    local keymap = vim.keymap.set
+    keymap('n', '<leader>uu', '<cmd>DBUIToggle<cr>', { desc = "DBUI Toggle" })
+    keymap('n', '<leader>uf', '<cmd>DBUIFindBuffer<cr>', { desc = "DBUI Find Buffer" })
+    keymap('n', '<leader>uf', '<cmd>DBUILastQueryInfo<cr>', { desc = "DBUI Last Query Info" })
+  end,
+  config = function()
+  end
 }
